744f0e0925c78bd356abd6a0b59ae979457f3eee
IsthisAlif <abdulalif2003@gmail.com>
2026-06-17 01:51:27 +0800

    commit 5

--- Tutorial/Filtering/Filtering.py
+++ Tutorial/Filtering/Filtering.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv("Tutorial/Filtering/pokedex.csv")
+
+# Filtering = Keeping the rows that match a condition
+
+#tall_pokemon = df[df["Height"] >= 2]
+#heavy_pokemon = df[df["Weight"] > 100]
+#legendary_pokemon = df[df["Legendary"] == True]
+
+#water_pokemon = df[(df["Type1"] == "Water") |
+#                   (df["Type2"] == "Water")]
+
+ff_pokemon = df[(df["Type1"] == "Fire") &
+                (df["Type2"] == "Flying")]
+
+print(ff_pokemon)
